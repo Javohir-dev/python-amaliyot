aab8d31d98aa3947d5d5f55b659b7695450007d9
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-08-07 17:56:28 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,76 @@
+# Python Amaliyotlari
+
+
+# =============================================================================
+# # Enter the number you want.
+# # 45.2
+# # 45
+# # 45 ning kvadrati 2025 ga teng
+# # 45 ning kubi 91125 ga teng
+# 
+# def count_number():
+#     stop = True
+#     while stop:
+#         number = input('Hohlagan raqamingizni kiriting.\n>>> ')
+#         if number:
+#             number = float(number)
+#             number = int(number // 1)
+#             print(number)
+#             print(f"{number} ning kvadrati {number**2} ga teng")
+#             print(f"{number} ning kubi {number**3} ga teng")
+#             break
+#             
+#         else:
+#             print("==============================")
+#             print("\nBo'sh ma'mulot qabul qila olmaymiz.\n")
+#             print("==============================")
+# =============================================================================
+
+
+import datetime
+
+# =============================================================================
+# # Yoshingiz nechida?
+# # 22
+# # Siz 2001-yilda tug'ilgansiz.
+# def find_birthdate():
+#     now = datetime.datetime.now()
+#     stop = True
+#     while stop:
+#         age = input("Yoshingiz nechida?\n>>> ")
+#         if age:
+#             age = float(age)
+#             second_age = age // 1
+#             if second_age == age:
+#                 stop = False
+#                 age = int(age)
+#                 print(f"Siz {now.year - age}-yilda tug'ilgansiz.")
+#             else:
+#                 print("==============================")
+#                 print("\nIltimos butun son kiriting.\n")
+#                 print("==============================")
+#         else:
+#             print("==============================")
+#             print("\nBo'sh ma'mulot qabul qila olmaymiz.\n")
+#             print("==============================")
+# =============================================================================
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
