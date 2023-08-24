1ba1577716c8f0b814411bdf0482b0b4fd38838d
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-08-24 17:11:11 +0500

    python

--- test.py
+++ test.py
@@ -28,7 +28,6 @@
 
 
 import datetime
-
 # =============================================================================
 # # Yoshingiz nechida?
 # # 22
@@ -54,23 +53,73 @@ import datetime
 #             print("\nBo'sh ma'mulot qabul qila olmaymiz.\n")
 #             print("==============================")
 # =============================================================================
-            
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 
+
+# Funksiyalar
+def full_name(first_name, last_name="kiritilmagan"):
+    fullname = f"{first_name} {last_name}"
+    
+    return fullname
+    
+fullname = full_name("javohir", "khamidullaev") 
+# print(fullname.title())
+fullname = full_name(last_name="qahramoniy", first_name="azam") 
+# print(fullname.title()) 
+
+
+def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+    avto = {
+        'kompaniya':kompaniya,
+        'model':model,
+        'rang':rangi,
+        'korobka':korobka,
+        'yil':yili,
+        'narh':narhi
+    }
+    
+    return avto
+
+
+avto1 = avto_info('GM','Malibu','Qora','Avtomat',2018)
+avto2 = avto_info('GM','Gentra','Oq','Mexanika',2016,15000)
+avtolar = [avto1, avto2]
+
+print('Onlayn bozordagi mavjud avtomashinalar:')
+
+for avto in avtolar:
+    if avto['narh']:
+        narh = avto['narh']
+    else:
+        narh = "Noma'lum"
+    print(f"{avto['rang']} {avto['model']}. Narhi: {narh}")
+
+
+def my_range(min, max, step=1):
+    sonlar = [] # bo'sh ro'yxat
+    while min < max:
+        sonlar.append(min)
+        min += step
+    return sonlar
+
+
+lst = my_range(0, 20, 2)
+
+
+def num(one, two, three):
+    n = {
+        'one': one,
+        'two': two,
+        'three': three ,   
+    }
+    
+    return n
+numbers = []
+one = 1
+two = 2
+three = 3
+numbers.append(num(one, two, three))
+    
+one = 10
+two = 20
+three = 30
+numbers.append(num(one, two, three))
